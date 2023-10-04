2434ad63ad2ea16f7e414a5a86c3a7a18a98e6dd
Rayy-007 <minkhant88887@gmail.com>
2023-10-04 16:50:07 +0630

    Styled Nabar and Responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { ImCross } from "react-icons/im";
+import { TiThMenuOutline } from "react-icons/ti";
 import "./navbar.css";
 import logo from "../../assets/logo1.png";
 
 function Navbar() {
+  const [open, setOpen] = React.useState(false);
+
+  const styles = {
+    transform: open && `translateX(0%)`,
+  };
+
   return (
     <div className="navbar">
       <a className="logo" href="#home">
@@ -10,7 +18,7 @@ function Navbar() {
         <h4>Khant</h4>
       </a>
 
-      <ul className="links">
+      <ul style={styles} className="links">
         <li>
           <a className="active" href="#header">
             Home
@@ -30,6 +38,11 @@ function Navbar() {
       <div className="cta">
         <button className="btn btn-outline">Contact Me</button>
       </div>
+
+      {/* Mobile Close and Menu */}
+      <TiThMenuOutline onClick={() => setOpen(true)} className="menu" />
+
+      {open && <ImCross onClick={() => setOpen(false)} className="close" />}
     </div>
   );
 }
