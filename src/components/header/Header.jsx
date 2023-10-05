71131ce45cd93643ee6f3be908adf8546174c68a
Rayy-007 <minkhant88887@gmail.com>
2023-10-05 12:50:42 +0630

    Created and Styled Responsive Header Section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./header.css";
+import ImageMe from "../../assets/Me-1.png";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="container section-padding-1 header-container">
+      <div className="header-left">
+        <h4>Hey, I am Min Khant</h4>
+        <h1>
+          <span>
+            <span className="title-small">I am </span> Front-End{" "}
+            <span className="title-small">Developer and</span> UI/UX{" "}
+            <span className="title-small">Designer.</span>{" "}
+          </span>
+        </h1>
+        <p className="small">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi, Lorem
+          ipsum, dolor sit amet consectetur adipisicing elit. Modi, magnam.
+        </p>
+        <a className="btn btn-primary" href="#">
+          Get In Touch
+        </a>
+      </div>
+      <div className="header-right">
+        <img src={ImageMe} alt="Photo of Me" />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
