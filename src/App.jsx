d773019e37966c556c105128a5e2cda4bc7a9ee5
Rayy-007 <minkhant88887@gmail.com>
2023-10-10 17:37:20 +0630

    Fixed Bugs responsive and Created and Styled the Testimonials Section

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       <MySkill />
       <AboutMe />
       <Project />
+      <Testimonial />
     </>
   );
 }
--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,8 +1,58 @@
 import React from "react";
 import "./testimonial.css";
+import person1 from "../../assets/person-1.png";
+import stars from "../../assets/stars.png";
 
 function Testtmonial() {
-  return <div className="testimonial"></div>;
+  const testimonialsData = [
+    {
+      feedback:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.",
+      name: "Dianne Russell",
+      job: "Starbucks",
+      image: person1,
+    },
+    {
+      feedback:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.",
+      name: "Dianne Russell",
+      job: "Starbucks",
+      image: person1,
+    },
+    {
+      feedback:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.",
+      name: "Dianne Russell",
+      job: "Starbucks",
+      image: person1,
+    },
+  ];
+
+  const testimonialsCard = testimonialsData.map((data, index) => (
+    <div key={index} className="card">
+      <img className="stars" src={stars} alt="Stars" />
+      <p className="extra-small">{data.feedback}</p>
+      <div className="customer">
+        <img src={data.image} alt="User" />
+        <div className="customer-info">
+          <h4>{data.name}</h4>
+          <p>{data.job}</p>
+        </div>
+      </div>
+    </div>
+  ));
+
+  return (
+    <div id="Testimonials" className="testimonial">
+      <div className=" testimonial-container container section-padding-2">
+        <div className="title">
+          <h4>Client Feedback</h4>
+          <h2>Customer testimonials</h2>
+        </div>
+        <div className="cards">{testimonialsCard}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Testtmonial;
