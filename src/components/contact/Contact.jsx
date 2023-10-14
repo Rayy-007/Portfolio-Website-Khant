e69587f0bcb8571798edd69c344ab9c7b4ca630e
Rayy-007 <minkhant88887@gmail.com>
2023-10-14 22:10:26 +0630

    Created the Contact Me Section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,50 @@ import React from "react";
 import "./contact.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="contact container section-padding-2">
+      <div className="title">
+        <h4>Get In Touch</h4>
+        <h2>Contact me</h2>
+        <p className="extra-small">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        </p>
+      </div>
+      <div className="form">
+        <div className="field">
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" />
+        </div>
+        <div className="field">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" />
+        </div>
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" />
+        </div>
+        <div className="field">
+          <label htmlFor="ph-number">Phone Number</label>
+          <input type="text" id="ph-number" />
+        </div>
+        <div className="field">
+          <label htmlFor="topic">Choose a topic</label>
+          <select id="topic">
+            <option value="">Select One..</option>
+          </select>
+        </div>
+        <div className="field">
+          <label htmlFor="message">Message</label>
+          <textarea id="message" placeholder="Type your message..."></textarea>
+        </div>
+        <div className="field">
+          <input type="checkbox" id="accept" />
+          <label htmlFor="accept">I accept the terms</label>
+        </div>
+        <input type="submit" value="Submit" />
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
