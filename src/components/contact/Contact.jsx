92d1efcd0da75e37f5d153dbe5ccdf784ce62589
Rayy-007 <minkhant88887@gmail.com>
2023-10-17 13:26:40 +0630

    Styled Responsive Contact Section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,34 +13,48 @@ function Contact() {
       </div>
       <div className="form">
         <div className="field">
-          <label htmlFor="first-name">First Name</label>
+          <label className="extra-small" htmlFor="first-name">
+            First Name
+          </label>
           <input type="text" id="first-name" />
         </div>
         <div className="field">
-          <label htmlFor="last-name">Last Name</label>
+          <label className="extra-small" htmlFor="last-name">
+            Last Name
+          </label>
           <input type="text" id="last-name" />
         </div>
         <div className="field">
-          <label htmlFor="email">Email</label>
+          <label className="extra-small" htmlFor="email">
+            Email
+          </label>
           <input type="text" id="email" />
         </div>
         <div className="field">
-          <label htmlFor="ph-number">Phone Number</label>
+          <label className="extra-small" htmlFor="ph-number">
+            Phone Number
+          </label>
           <input type="text" id="ph-number" />
         </div>
         <div className="field">
-          <label htmlFor="topic">Choose a topic</label>
+          <label className="extra-small" htmlFor="topic">
+            Choose a topic
+          </label>
           <select id="topic">
             <option value="">Select One..</option>
           </select>
         </div>
         <div className="field">
-          <label htmlFor="message">Message</label>
+          <label className="extra-small" htmlFor="message">
+            Message
+          </label>
           <textarea id="message" placeholder="Type your message..."></textarea>
         </div>
-        <div className="field">
+        <div className="field accept">
           <input type="checkbox" id="accept" />
-          <label htmlFor="accept">I accept the terms</label>
+          <label className="extra-small" htmlFor="accept">
+            I accept the terms
+          </label>
         </div>
         <input type="submit" value="Submit" />
       </div>
