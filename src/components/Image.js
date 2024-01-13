8525f8b7ffcb6b20f5579e8b60faa0437066a70d
Rayy-007 <minkhant88887@gmail.com>
2024-01-13 18:03:53 +0630

    added new skill and styling

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,5 @@
 import aboutMe from "../assets/about-me.png";
 import arrow from "../assets/arrow.png";
-import frontDev from "../assets/front-dev.png";
 import github from "../assets/github.png";
 import logo1 from "../assets/logo1.png";
 import Me from "../assets/Me-1.png";
@@ -10,10 +9,31 @@ import person3 from "../assets/person3.png";
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
-import reactLogo from "../assets/react-logo.png";
 import stars from "../assets/stars.png";
+
+// Skills
+import frontDev from "../assets/front-dev.png";
 import strategy from "../assets/strategy.png";
 import uiUX from "../assets/ui-ux.png";
+import fullstack from "../assets/skills/fullstack.png";
+// Frontend Skills
+import reactLogo from "../assets/react-logo.png";
+import nextjs from "../assets/skills/nextjs.png";
+import javascript from "../assets/skills/javascript.png";
+import tailwind from "../assets/skills/tailwind.png";
+import jquery from "../assets/skills/jquery.png";
+import sass from "../assets/skills/sass.png";
+import bootstrap from "../assets/skills/bootstrap.png";
+// Backend Skills
+import springboot from "../assets/skills/springboot.png";
+import java from "../assets/skills/java.png";
+import mysql from "../assets/skills/mysql.png";
+import mongodb from "../assets/skills/mongodb.png";
+import firebase from "../assets/skills/firebase.png";
+// UI UX Skills
+import figma from "../assets/skills/figma.png";
+import prototype from "../assets/skills/prototype.png";
+import wireframe from "../assets/skills/wireframe.png";
 
 export {
   aboutMe,
@@ -32,4 +52,19 @@ export {
   stars,
   strategy,
   uiUX,
+  fullstack,
+  nextjs,
+  javascript,
+  tailwind,
+  jquery,
+  sass,
+  bootstrap,
+  springboot,
+  java,
+  mysql,
+  mongodb,
+  firebase,
+  figma,
+  prototype,
+  wireframe,
 };
