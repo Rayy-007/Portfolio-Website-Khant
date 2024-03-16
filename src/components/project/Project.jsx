c26bd7c8425d8f4f94e07319c9b444f77f682ddb
Rayy-007 <minkhant88887@gmail.com>
2024-03-16 22:50:15 +0630

    feat: implment tab and animated tab mover

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,85 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./project.css";
 
-import {
-  github,
-  arrow,
-  charity,
-  dashboard,
-  dobu,
-  ecommerce,
-  quizzical,
-} from "../Image";
+import { github, arrow } from "../Image";
 import ScrollAnimation from "../utils/ScrollAnimation";
+import { projectsData } from "./projectsData";
 
 function Project() {
-  // Projects Data for Projects Card
-  const projectsData = [
-    {
-      title: "React Quizzical App",
-      description:
-        "🤹‍♀️Quizzical App is a dynamic React application that challenges users with a variety of quizzes sourced from an external quiz API.",
-      codeUrl: "https://github.com/Rayy-007/React-Quizzical-App-by-Khant",
-      websiteURl: "https://react-quizzical-app-khant.netlify.app/",
-      image: quizzical,
-    },
-    {
-      title: "DoBu Martial Arts",
-      description:
-        "🏋️‍♀️DoBu Martial Arts Project is a visually appealing website crafted entirely with my own design.",
-      codeUrl: "https://github.com/Rayy-007/DoBu-Martial-arts-project",
-      websiteURl: "https://dobumartialarts.netlify.app/",
-      image: dobu,
-    },
-    {
-      title: "Dashboard Website",
-      description:
-        "🎨Dashboard Simple Project is a sleek and modern web application designed as an admin dashboard.",
-      codeUrl: "https://github.com/Rayy-007/dashboard-simple-project",
-      websiteURl: "https://dashboard-simple-project.netlify.app/",
-      image: dashboard,
-    },
-    {
-      title: "Charity Organization Website",
-      description:
-        "⚡ Full-stack development project for MerryMeal - Charity Organization.",
-      codeUrl: "https://github.com/Rayy-007/meals-on-wheel-project",
-      websiteURl: "https://charity-organization-by-khant.netlify.app/",
-      image: charity,
-    },
-    {
-      title: "Ecommerce Project",
-      description:
-        "🛒This Ecommerce Project is a comprehensive web application focused on building a responsive ecommerce website.",
-      codeUrl: "https://github.com/Rayy-007/Ecommerce-Website-By-Khant",
-      websiteURl: "https://multi-page-ecommerce-by-khant.netlify.app/",
-      image: ecommerce,
-    },
-  ];
-
-  // const [repoDetails, setRepoDetails] = useState(null);
-  // useEffect(() => {
-  //   const fetchRepoDetails = async () => {
-  //     try {
-  //       const response = await fetch(import.meta.env.VITE_GITHUB_codeUrl);
-
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch repository details");
-  //       }
-
-  //       const data = await response.json();
-  //       setRepoDetails(data);
-  //       // console.log(data[0].owner.avatar_codeUrl);
-  //       console.log(data[0].topics);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   };
-
-  //   fetchRepoDetails();
-  // }, []);
-
-  // Creating the Projects Card
+  const [projectTabMenu, setProjectTabMenu] = useState("all");
 
   const recentProjectsCard = projectsData.slice(0, 2).map((data, index) => (
     <div key={index} className=" recent project-card">
@@ -126,6 +53,14 @@ function Project() {
     </div>
   ));
 
+  const calculateTabMoverPosition = () => {
+    if (projectTabMenu === "all") return "0%";
+    if (projectTabMenu === "website") return "25%";
+    if (projectTabMenu === "app") return "50%";
+    if (projectTabMenu === "uiux") return "75%";
+    return "0%";
+  };
+
   return (
     <div id="Projects" className="projects container section-padding-2">
       <div className="top flex-row">
@@ -145,6 +80,39 @@ function Project() {
         </a>
       </div>
 
+      <div className="project-tab">
+        <ul>
+          <div
+            className="tab-mover"
+            style={{ left: calculateTabMoverPosition() }}
+          ></div>
+          <li
+            className={projectTabMenu === "all" ? "active" : ""}
+            onClick={() => setProjectTabMenu("all")}
+          >
+            All
+          </li>
+          <li
+            className={projectTabMenu === "website" ? "active" : ""}
+            onClick={() => setProjectTabMenu("website")}
+          >
+            Websites
+          </li>
+          <li
+            className={projectTabMenu === "app" ? "active" : ""}
+            onClick={() => setProjectTabMenu("app")}
+          >
+            App
+          </li>
+          <li
+            className={projectTabMenu === "uiux" ? "active" : ""}
+            onClick={() => setProjectTabMenu("uiux")}
+          >
+            UI/UX
+          </li>
+        </ul>
+      </div>
+
       <div className="projects-con">{recentProjectsCard}</div>
       <div className="projects-con">{projectsCard}</div>
       <a
@@ -159,3 +127,28 @@ function Project() {
 }
 
 export default Project;
+
+// ? GitHub API Tested
+// const [repoDetails, setRepoDetails] = useState(null);
+// useEffect(() => {
+//   const fetchRepoDetails = async () => {
+//     try {
+//       const response = await fetch(import.meta.env.VITE_GITHUB_codeUrl);
+
+//       if (!response.ok) {
+//         throw new Error("Failed to fetch repository details");
+//       }
+
+//       const data = await response.json();
+//       setRepoDetails(data);
+//       // console.log(data[0].owner.avatar_codeUrl);
+//       console.log(data[0].topics);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   fetchRepoDetails();
+// }, []);
+
+// Creating the Projects Card
---
+++ src/components/project/projectsData.js
@@ -0,0 +1,45 @@
+import { charity, dashboard, dobu, ecommerce, quizzical } from "../Image";
+
+// Projects Data for Projects Card
+export const projectsData = [
+  {
+    title: "React Quizzical App",
+    description:
+      "🤹‍♀️Quizzical App is a dynamic React application that challenges users with a variety of quizzes sourced from an external quiz API.",
+    codeUrl: "https://github.com/Rayy-007/React-Quizzical-App-by-Khant",
+    websiteURl: "https://react-quizzical-app-khant.netlify.app/",
+    image: quizzical,
+  },
+  {
+    title: "DoBu Martial Arts",
+    description:
+      "🏋️‍♀️DoBu Martial Arts Project is a visually appealing website crafted entirely with my own design.",
+    codeUrl: "https://github.com/Rayy-007/DoBu-Martial-arts-project",
+    websiteURl: "https://dobumartialarts.netlify.app/",
+    image: dobu,
+  },
+  {
+    title: "Dashboard Website",
+    description:
+      "🎨Dashboard Simple Project is a sleek and modern web application designed as an admin dashboard.",
+    codeUrl: "https://github.com/Rayy-007/dashboard-simple-project",
+    websiteURl: "https://dashboard-simple-project.netlify.app/",
+    image: dashboard,
+  },
+  {
+    title: "Charity Organization Website",
+    description:
+      "⚡ Full-stack development project for MerryMeal - Charity Organization.",
+    codeUrl: "https://github.com/Rayy-007/meals-on-wheel-project",
+    websiteURl: "https://charity-organization-by-khant.netlify.app/",
+    image: charity,
+  },
+  {
+    title: "Ecommerce Project",
+    description:
+      "🛒This Ecommerce Project is a comprehensive web application focused on building a responsive ecommerce website.",
+    codeUrl: "https://github.com/Rayy-007/Ecommerce-Website-By-Khant",
+    websiteURl: "https://multi-page-ecommerce-by-khant.netlify.app/",
+    image: ecommerce,
+  },
+];
